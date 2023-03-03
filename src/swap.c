acc883a29e9ac2ece53121572cb56789b1c3346a
Sungoh Kim <sungohki@c6r2s6.42seoul.kr>
2023-03-03 18:45:14 +0900

    fixed : function contents fixed

--- src/swap.c
+++ src/swap.c
@@ -6,32 +6,42 @@
 /*   By: sungohki <sungohki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 14:43:26 by sungohki          #+#    #+#             */
-/*   Updated: 2023/03/03 18:22:01 by sungohki         ###   ########.fr       */
+/*   Updated: 2023/03/03 18:40:16 by sungohki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+static void	swap_node(t_list *ex)
+{
+	t_list		*ex2;
+	t_list		*ex3;
+
+	if (ft_lstsize(ex) > 1)
+	{
+		ex2 = ex->next;
+		ex3 = ex2->next;
+		ex->next = ex3;
+		ex2->next = ex;
+		ex = ex2;
+	}
+}
+
 void	sa(t_list *a)
 {
-	t_list		*temp;
-
-	temp->next = a->next->next;
-	a->next->next = a;
-	a->next = temp->next;
+	swap_node(a);
+	write(1, "sa\n", 3);
 }
 
 void	sb(t_list *b)
 {
-	t_list		*temp;
-
-	temp->next = b->next->next;
-	b->next->next = b;
-	b->next = temp->next;
+	swap_node(b);
+	write(1, "sa\n", 3);
 }
 
 void	ss(t_list *a, t_list *b)
 {
-	sa(a);
-	sb(b);
+	swap_node(a);
+	swap_node(b);
+	write(1, "ss\n", 3);
 }
