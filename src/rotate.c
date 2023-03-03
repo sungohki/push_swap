a685cfc43fef0a29f5c2f1d3ea0ba2ec0b3b35c6
Sungoh Kim <sungohki@c6r2s6.42seoul.kr>
2023-03-03 19:03:27 +0900

    modified : add functions' contents

--- src/rotate.c
+++ src/rotate.c
@@ -6,23 +6,40 @@
 /*   By: sungohki <sungohki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 14:48:38 by sungohki          #+#    #+#             */
-/*   Updated: 2023/02/28 14:53:14 by sungohki         ###   ########.fr       */
+/*   Updated: 2023/03/03 19:01:23 by sungohki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ra(void)
+static void	rotate_node(t_list *ex)
 {
+	t_list	*temp;
+	t_list	*ex_2nd;
+	t_list	*ex_last;
 
+	ex_2nd = ex->next;
+	ex_last = ft_lstlast(ex);
+	ex->next = NULL;
+	ex_last->next = ex;
+	ex = ex_2nd;
 }
 
-void	rb(void)
+void	ra(t_list *a)
 {
-
+	rotate_node(a);
+	write(1, "ra\n", 3);
 }
 
-void	rr(void)
+void	rb(t_list *b)
 {
+	rotate_node(b);
+	write(1, "rb\n", 3);
+}
 
+void	rr(t_list *a, t_list *b)
+{
+	rotate_node(a);
+	rotate_node(b);
+	write(1, "rr\n", 3);
 }
