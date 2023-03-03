2c6e0bdfc6ecb7b9fd17db39dc23b56ec1c314d9
Sungoh Kim <sungohki@c6r2s6.42seoul.kr>
2023-03-03 18:22:18 +0900

    mofied : write functions

--- src/swap.c
+++ src/swap.c
@@ -6,23 +6,32 @@
 /*   By: sungohki <sungohki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 14:43:26 by sungohki          #+#    #+#             */
-/*   Updated: 2023/02/28 14:47:59 by sungohki         ###   ########.fr       */
+/*   Updated: 2023/03/03 18:22:01 by sungohki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	sa(void)
+void	sa(t_list *a)
 {
+	t_list		*temp;
 
+	temp->next = a->next->next;
+	a->next->next = a;
+	a->next = temp->next;
 }
 
-void	sb(void)
+void	sb(t_list *b)
 {
+	t_list		*temp;
 
+	temp->next = b->next->next;
+	b->next->next = b;
+	b->next = temp->next;
 }
 
-void	ss(void)
+void	ss(t_list *a, t_list *b)
 {
-
+	sa(a);
+	sb(b);
 }
